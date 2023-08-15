d0d55916215a92203fc34f0e6262c97f2f4bc3cd
Thaila Alves <thailasalvees@gmail.com>
2023-08-15 11:01:15 -0300

    Falta resolver as questões 10 e a 13

--- lista041/questao08.py
+++ lista041/questao08.py
@@ -5,3 +5,8 @@ se ele está na faixa de 1 a 10.
 
 num = int(input("Informe um número: "))
 
+if (num < 10 and num > 1):
+    print("Este número esta na faixa de 1 a 10")
+
+else:
+    print("Este número não esta na faixa de 1 a 10")
--- lista041/questao09.py
+++ lista041/questao09.py
@@ -2,3 +2,14 @@
 Desenvolver um programa que pergunte um número e exiba a informação de que ele é positivo, negativo ou
 nulo.
 '''
+
+num = int(input("Informe um número aqui: "))
+
+if (num < 0):
+    print("Este número é negativo")
+
+if (num == 0):
+    print("Este número é nulo")
+
+if (num > 0):
+    print("Este número é positivo")
